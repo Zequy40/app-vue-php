80a58dc84d1d1f830e3f3b807399d189996cd6d1
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2024-03-15 10:56:09 +0100

    app created with vue in the frontend and php in the backend

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { ref, defineProps, createApp, onMounted } from 'vue'
+import axios from 'axios'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+const cliente = ref([])
+const apiUrl = 'http://localhost:8080/'
+
+const cargarClientes = async () => {
+  const respuesta = await axios.get(apiUrl)
+  cliente.value = respuesta.data 
+}
+const newClient = ref({name:'', lastname:'', mail:''})
+const createClient = async () => {
+  try{
+    const resp = await axios.post(apiUrl,newClient.value)
+    cliente.value.push(resp.data);
+    alert(`El Cliente fue creado exitosamente 🥳🥳`)
+    newClient.value={name:'', lastname:'', mail:''}
+    cargarClientes()
+    
+  }catch(error){
+    console.log('Error al crear el Cliente')
+    alert("Ocurrio un error al crear el Cliente")
+  }
+}
+const deleteClient  = async (id) => {
+  
+  let confirmacion=confirm("¿Desea eliminar este registro?");
+  if (!confirmacion) return;
+  else{
+    try{
+      await axios.delete(`${apiUrl}${id}` )
+      cargarClientes()
+      
+    } catch(error){
+      console.log('No se pudo eliminar el Cliente')
+      alert ("No se pudo eliminar el Cliente")
+    }
+  }
+}
+
+onMounted(() => {
+  cargarClientes()
+})
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div id="app">
+    
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-6"
+          >
+          <div class="card">
+            <div class="card-header">Clientes</div>
+            <div class="card-body">
+              <form @submit.prevent="createClient"
+              >
+              Nombre:
+              <input class="form-control" type="text" name="name" v-model="newClient.name" id="name" required>
+              Apellidos:
+              <input class="form-control" type="text" name="lastname" v-model="newClient.lastname" id="lastname" required>
+              Email:
+              <input class="form-control" type="email" name="email" v-model="newClient.mail" id="email" required><br><br>
+              <button class="btn btn-success" type="submit">Crear Cliente</button>
+              </form>
+             
+            </div>
+            <div class="card-footer">ZekiDev</div>
+          </div>
+           
+          </div>
+          <div
+            class="col-6"
+          >
+          <div class="card">
+            <div class="card-header">Lista de los clientes</div>
+            <div class="card-body">
+              <ul class="list-group mt-3">
+                <li class="list-group-item d-flex align-items-center" v-for="clientes in cliente" :key="clientes.id"> 
+                  <button class="btn btn-danger btn-sm" @click="deleteClient(clientes.id)">Eliminar</button>
+                  &nbsp;
+                  &nbsp;
+                  {{ clientes.name }}  {{ clientes.lastname }}
+                  &nbsp;
+                  <span> - Email: {{ clientes.mail }}</span>
+                 
+                </li>
+              </ul>
+              
+            </div>
+            <div class="card-footer text-muted"></div>
+          </div>
+          
+            
+          </div>
+          
+        </div>
+      
+      
+
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
